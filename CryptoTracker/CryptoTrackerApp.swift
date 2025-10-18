6fadc620c5c4caffffca30bfdf5f3afcb385aa16
Shakawat_Ratul <62506442+Ratul97@users.noreply.github.com>
2025-10-19 01:14:15 +0600

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Kazi Shakawat Hossain Ratul on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
