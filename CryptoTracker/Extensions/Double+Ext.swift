b3bae7c5fffa7b0cac76d7d391be4fe74fbd087a
Shakawat_Ratul <62506442+Ratul97@users.noreply.github.com>
2025-10-27 01:35:59 +0600

    updated currency formatter

--- CryptoTracker/Extensions/Double+Ext.swift
+++ CryptoTracker/Extensions/Double+Ext.swift
@@ -10,14 +10,17 @@ import Foundation
 extension Double {
     private var numberFormatter: NumberFormatter {
         let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.currencySymbol = "$"
         numberFormatter.minimumFractionDigits = 2
         numberFormatter.maximumFractionDigits = 2
+        numberFormatter.locale = Locale(identifier: "en_US")
         return numberFormatter
     }
     
     func asDecimalCurrency() -> String {
         let number = NSNumber(value: self)
-        return numberFormatter.string(from: number) ?? "0.00"
+        return numberFormatter.string(from: number) ?? "$0.00"
     }
     
     func asCurrencyWithPercent() -> String {
